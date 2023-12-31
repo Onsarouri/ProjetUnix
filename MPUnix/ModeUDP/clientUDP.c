bd9708e36ec8fb6e7d07973ab491f861886c9a39
Onsarouri <72884666+Onsarouri@users.noreply.github.com>
2023-12-31 21:02:45 +0100

    Update clientUDP.c

--- MPUnix/ModeUDP/clientUDP.c
+++ MPUnix/ModeUDP/clientUDP.c
@@ -3,94 +3,64 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <signal.h>
 
 #define NMAX 10
 
-volatile sig_atomic_t timeout_flag = 0;
-
-void timeout_handler(int signo) {
-    timeout_flag = 1;
-}
-
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
-}
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server_address> <server_port>\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "Usage: %s <serveur_address> <serveur_port>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    const char *server_address = argv[1];
-    const int server_port = atoi(argv[2]);
+    const char *serveur_address = argv[1];
+    int serveur_port = atoi(argv[2]);
 
-    // Création de la socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        error("Erreur lors de la création de la socket");
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        perror("Erreur lors de la création du socket");
+        exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(server_port);
+    struct sockaddr_in serveur_addr;
+    memset(&serveur_addr, 0, sizeof(serveur_addr));
+    serveur_addr.sin_family = AF_INET;
+    serveur_addr.sin_port = htons(serveur_port);
+    inet_pton(AF_INET, serveur_address, &serveur_addr.sin_addr);
 
-    // Utilisation de getaddrinfo pour résoudre l'adresse du serveur
-    struct addrinfo hints, *result, *p;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-
-    if (getaddrinfo(server_address, NULL, &hints, &result) != 0) {
-        fprintf(stderr, "Erreur lors de la résolution de l'adresse du serveur\n");
-        close(sockfd);
-        exit(1);
+    int nombres[NMAX];
+    for (int i = 0; i < NMAX; ++i) {
+        nombres[i] = rand() % NMAX + 1;
     }
 
-    for (p = result; p != NULL; p = p->ai_next) {
-        if (p->ai_family == AF_INET) {
-            memcpy(&server_addr.sin_addr, &((struct sockaddr_in *)p->ai_addr)->sin_addr, sizeof(struct in_addr));
-            break;
-        }
+    printf("Nombres aléatoires générés par le client et envoyés au serveur:\n");
+    for (int i = 0; i < NMAX; ++i) {
+        printf("%d ", nombres[i]);
     }
-    freeaddrinfo(result);
+    printf("\n");
 
-    // Génération d'un nombre aléatoire
-    int n = rand() % NMAX + 1;
-
-    // Envoi du nombre au serveur
-    sendto(sockfd, &n, sizeof(n), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    // Set up a signal handler for timeout
-    struct sigaction sa;
-    sa.sa_handler = timeout_handler;
-    sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGALRM, &sa, NULL);
-
-    // Set a timeout (e.g., 2 seconds)
-    alarm(2);
-
-    // Réception de la réponse du serveur
-    int sorted_number;
-    recvfrom(sockfd, &sorted_number, sizeof(sorted_number), 0, NULL, NULL);
-
-    // Reset the alarm
-    alarm(0);
-
-    if (timeout_flag) {
-        printf("Timeout: No response from the server\n");
-    } else {
-        // Affichage de la réponse
-        printf("Nombre trié reçu du serveur: %d\n", sorted_number);
+    if (sendto(sock, nombres, sizeof(int) * NMAX, 0, (struct sockaddr *)&serveur_addr, sizeof(serveur_addr)) == -1) {
+        perror("Erreur lors de l'envoi des données au serveur");
+        close(sock);
+        exit(EXIT_FAILURE);
     }
 
-    // Fermeture de la socket
-    close(sockfd);
+    printf("Données envoyées avec succès au serveur.\n");
+
+    int nombres_tries[NMAX];
+
+    if (recvfrom(sock, nombres_tries, sizeof(int) * NMAX, 0, NULL, NULL) == -1) {
+        perror("Erreur lors de la réception des données triées du serveur");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Nombres triés reçus du serveur:\n");
+    for (int i = 0; i < NMAX; ++i) {
+        printf("%d ", nombres_tries[i]);
+    }
+    printf("\n");
+
+    close(sock);
 
     return 0;
 }
